3e4028976932f2cd22de7bf90fecd8d15f5b128e
Gonzalo <gonzaloadamie@gmail.com>
2023-05-16 21:05:50 -0300

    3.0V

--- src/assets/js/biografia.js
+++
@@ -1,8 +0,0 @@
-function agregarBiografia(){
-    document.getElementById("outputBiografia").style.display = "block";   
-    let text = document.getElementById("inputBiografia").innerText;
-
-    function biografia(valor){
-        document.getElementById("inputBiografia").innerText = valor;
-    }
-};
